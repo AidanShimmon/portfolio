000db510b30427727417694d04c647d3750ccdc1
AidanShimmon <aidanshimmon@hotmail.co.uk>
2021-01-01 15:00:56 +0000

    reworking the project panels

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,26 +1,73 @@
 import * as React from "react"
 // Components
 import styled from "styled-components";
+// Animation
+import { gsap } from "gsap";
 
 const Project = (data) => {
-    console.log(data.data.projects.projectTitle);
-    return(               
-        <StyledProjectContent style={{backgroundImage: `url(${data.data.projects.projectImage.sourceUrl})`}}>
-            <h3>{data.data.projects.projectTitle}</h3>
-        </StyledProjectContent>
+    const projectMouseMove = (e) => {
+        const x = (e.clientX - e.currentTarget.offsetParent.offsetLeft);
+        const y = e.pageY - (document.documentElement.scrollTop);
+        const in_min = 0;
+        const out_min = -5;
+        const out_max = 5;
+
+        const rotateY = (x - in_min) * (out_max - out_min) / (e.currentTarget.offsetWidth - in_min) + out_min;
+        const rotateX = (y - in_min) * (out_max - out_min) / (e.currentTarget.offsetHeight - in_min) + out_min;
+
+        gsap.to(e.currentTarget, {
+            rotationX: -rotateX,
+            rotationY: rotateY,
+            duration: 0.3
+        });
+    }
+
+    const projectMouseLeave = (e) => {
+        gsap.to(e.target, {
+            rotationX: 0,
+            rotationY: 0,
+            duration: 0.5
+        });
+    }
+    return(          
+        <StyledProjectImage 
+            onMouseMove={projectMouseMove} 
+            onMouseLeave={projectMouseLeave}  
+            style={{backgroundImage: `url(${data.data.projects.projectImage.sourceUrl})`}}
+        >
+            <div className="overlay"></div>
+            <div className="project__content">
+                <h2>{data.data.projects.projectTitle}</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores, laboriosam?</p>
+            </div>
+        </StyledProjectImage>
     )
 }
 
-const StyledProjectContent = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    pointer-events: none;
+const StyledProjectImage = styled.div`
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
     background-repeat: no-repeat;
     background-size: cover;
-    background-position: center;
+    background-position: left;
+
+    .overlay {
+        background-color: rgba(0, 0, 0, 0.3);
+        width: 100%;
+        height: 100%;
+        transform: translateZ(100px) scale(0.80);
+        pointer-events: none;
+    }
+
+    .project__content {
+        position: absolute;
+        bottom: 25px;
+        left: 30px;
+        color: white;
+        transform: translateZ(100px) scale(0.80);
+        pointer-events: none;
+    }
 `
 
 export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,43 +1,10 @@
 import * as React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 // Components
 import styled from "styled-components";
 import Project from "./Project"
-// Animation
-import { gsap } from "gsap";
 
 const Projects = () => {
-    const moveForce = 10; // max movement in pixels
-    const rotateForce = 5; // max rotaion in deg
-
-    const projectMouseMove = (e) => {
-        const docX = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        const docY = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-        const moveX = (e.pageX - docX/2) / (docX/2) * moveForce;
-        const moveY = (e.pageY - docY/2) / (docY/2) * moveForce;
-
-        const rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
-        const rotateX = ((e.pageY / docY * rotateForce*2) - rotateForce);
-
-        gsap.to(e.target, {
-            x: moveX,
-            y: moveY,
-            rotationX: rotateX,
-            rotateY: rotateY,
-            duration: 0.3
-        });
-    }
-
-    const projectMouseLeave = (e) => {
-        gsap.to(e.target, {
-            x: 0,
-            y: 0,
-            rotationX: 0,
-            rotateY: 0,
-            duration: 0.5
-        });
-    }
 
     const projectsData = useStaticQuery(graphql`
         query {
@@ -63,10 +30,7 @@ const Projects = () => {
             <h1>Projects</h1>
             <div className="projects__grid">
                 {projectsData.allWpPost.edges.map(({ node }) => (
-                    <div
-                        onMouseMove={projectMouseMove} 
-                        onMouseLeave={projectMouseLeave}  
-                        className="project">
+                    <div className="project">
                         <Project data={node} />
                     </div>      
                 ))}
@@ -88,10 +52,8 @@ position: relative;
 
 .project {
     position: relative;
-    background-color: white;
     height: auto;
-    box-shadow: 0 0 40px 20px rgba(0,0,0,.15);
-    overflow: hidden;
+    perspective: 500px;
 
     :nth-child(1) {
         grid-column: 6/13;
